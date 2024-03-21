1c45cab89e6e4064348c955102ef065f5afbab02
Autherain <autherain@gmail.com>
2024-03-21 16:42:15 +0100

    lil' project working

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,63 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { stringify, v4 as uuidV4 } from "uuid"
 
-import confetti from 'canvas-confetti';
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+  createdAt: Date
+}
+
+console.log(uuidV4())
+
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.querySelector<HTMLFormElement>("#new-task-form")
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+
+const array_task: Task[] = load_task()
+array_task.forEach(add_list_item)
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+
+  if (input?.value == "" || input?.value == null) return
+
+  const new_task: Task = {
+    id: uuidV4(),
+    title: input?.value,
+    completed: false,
+    createdAt: new Date(),
+  }
+  array_task.push(new_task)
+  save_task(array_task)
+
+  add_list_item(new_task)
+
+  input.value = ""
+})
+
+function add_list_item(task: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    save_task(array_task)
+  })
+
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
+
+function save_task(array_task: Task[]) {
+  localStorage.setItem("TASKS", JSON.stringify(array_task))
+}
+
+function load_task(): Task[] {
+  const getted_task = localStorage.getItem("TASKS")
+  if (getted_task == null) return []
+  return JSON.parse(getted_task)
+}
